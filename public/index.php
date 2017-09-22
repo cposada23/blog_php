67e6c2cb04455960590a2960a99672ab20c9b756
Camilo Posada <cposadaa@gmail.com>
2017-09-22 15:09:20 -0500

    Rederizando las vistas desde un metodo

--- public/index.php
+++ public/index.php
@@ -5,6 +5,18 @@ error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 include_once    '../config.php';
 
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+
+// var_dump($baseUrl);
+
+function render($filename, $params = []) {
+    ob_start();
+    extract($params);
+    include $filename;
+    return ob_get_clean();
+}
+
 $route = $_GET['route'] ?? '/';
 
 use Phroute\Phroute\RouteCollector;
@@ -16,9 +28,11 @@ $router->get('/', function() use ($pdo) {
     $query->execute();
 
     $blogPosts = $query->fetchAll(PDO::FETCH_ASSOC);
-    include '../views/index.php';
+    return render('../views/index.php', ['blogPosts'=>$blogPosts]);
 });
 
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
 $response = $dispatcher->dispatch($_SERVER['REQUEST_METHOD'], $route);
+
+echo $response;
